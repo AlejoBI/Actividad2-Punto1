c2426a57667924ab432eda1618f85b6fe8a0a293
Alejandro Bravo Isajar <alejandrobravoisajar1@gmail.com>
2025-10-16 11:20:46 -0500

    Añadir Main

--- Actividad2-Punto1/src/main/java/com/uao/actividad2/punto1/Main.java
+++ Actividad2-Punto1/src/main/java/com/uao/actividad2/punto1/Main.java
@@ -1,6 +1,18 @@
 
 package com.uao.actividad2.punto1;
 
+
+import controller.Controller;
+import model.Competencia;
+import view.View;
+
 public class Main {
-    
+	public static void main(String[] args) {
+		// Inicializar MVC
+		Competencia competencia = new Competencia("Mundial Pista - Cali");
+		Controller controller = new Controller(competencia);
+		View view = new View();
+		// Delegar interacción con el usuario a la vista
+		view.iniciarMenu(controller);
+	}
 }
